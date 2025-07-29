035f932aa066544cc2593132458ad1645441b9b2
Pritom Das <pritom.gps@gmail.com>
2025-07-29 21:45:49 +0530

    add

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+       
+    cout<<12+13<<endl;
+    return 0;
+}
